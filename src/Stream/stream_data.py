ec80f08b1090d275a38daabed8ab7d90c0609d64
Attention Perception and Performance Lab Air1 <zrselk@gmail.com>
2019-03-23 16:48:25 -0600

    Added circular buffer to filtering

--- src/Stream/stream_data.py
+++ src/Stream/stream_data.py
@@ -21,24 +21,43 @@ stream = streams[0]
 
 inlet = StreamInlet(stream, max_chunklen=BUFFER)
 
+class CircularBuffer:
+    def __init__(self, chunks):
+        self.window = np.zeros((5, 256*chunks))
+        self.chunks = chunks
+        self.window_read = self.chunks // 2
+        self.window_write = 0
+        self.chunk_size = 256
 
+    def read(self):
+        #return self.window[:][self.window_read*self.chunk_size:(self.window_read+1)*self.chunk_size]
+        return self.window
+
+    def write(self, data):
+        self.window[:, self.window_write*self.chunk_size:(self.window_write+1)*self.chunk_size] = data
+        self.window_write = (self.window_write + 1) % self.chunks
+        self.window_read = (self.window_read + 1) % self.chunks
 
 times = []
+count = 0
+chunks = 3
+buf = CircularBuffer(chunks)
+
 while True:
     # Sample is a 2d array of [ [channel_i]*5 ] * BUFFER
     samples, timestamps = inlet.pull_chunk(timeout=2.0, max_samples=BUFFER)
     if timestamps:
         data = np.vstack(samples)
         data = np.transpose(data)
-        #print(np.transpose(data))
-        #print(np.shape(data))
-        print(len(data[0]))
-        channels = list([0])
-        lower_freq = 5
-        higher_freq = 15
-        filtered_data = mne.filter.filter_data(data, 256, lower_freq, higher_freq, filter_length=128)
-        #filtered_data1 = mne.filter.filter_data(data[1], 256, 5, 15)
-        #filtered_data2 = mne.filter.filter_data(data[2], 256, 5, 15)
-        #filtered_data3 = mne.filter.filter_data(data[3], 256, 5, 15)
-        #filtered_data4 = mne.filter.filter_data(data[4], 256, 5, 15)
-        print(filtered_data)
+        print(np.shape(data))
+        buf.write(data)
+
+        # Check so that the buffer is filled before any filtering
+        if count >= chunks:
+            channels = list([0])
+            lower_freq = 5
+            higher_freq = 15
+            filtered_data = mne.filter.filter_data(buf.window, 256, lower_freq, higher_freq, filter_length=256*chunks-1)
+            print(filtered_data)
+
+        count += 1
