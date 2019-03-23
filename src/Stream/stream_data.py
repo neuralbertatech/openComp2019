19a79ef4a5bd464fd7a3b9ccf3d08bca68cf7152
Attention Perception and Performance Lab Air1 <zrselk@gmail.com>
2019-03-23 15:15:09 -0600

    A bit of work on data streaming

--- src/Stream/stream_data.py
+++ src/Stream/stream_data.py
@@ -4,8 +4,10 @@ from scipy.signal import butter, lfilter, lfilter_zi, firwin
 from time import sleep
 from pylsl import StreamInlet, resolve_byprop
 from threading import Thread
+import mne
 
-BUFFER = 12
+# Enough for 1 sec at 256 Hz
+BUFFER = 256
 
 
 print("looking for an EEG stream...")
@@ -19,9 +21,24 @@ stream = streams[0]
 
 inlet = StreamInlet(stream, max_chunklen=BUFFER)
 
+
+
 times = []
 while True:
     # Sample is a 2d array of [ [channel_i]*5 ] * BUFFER
-    samples, timestamps = inlet.pull_chunk(timeout=1.0, max_samples=12)
+    samples, timestamps = inlet.pull_chunk(timeout=2.0, max_samples=BUFFER)
     if timestamps:
-        print(samples)
+        data = np.vstack(samples)
+        data = np.transpose(data)
+        #print(np.transpose(data))
+        #print(np.shape(data))
+        print(len(data[0]))
+        channels = list([0])
+        lower_freq = 5
+        higher_freq = 15
+        filtered_data = mne.filter.filter_data(data, 256, lower_freq, higher_freq, filter_length=128)
+        #filtered_data1 = mne.filter.filter_data(data[1], 256, 5, 15)
+        #filtered_data2 = mne.filter.filter_data(data[2], 256, 5, 15)
+        #filtered_data3 = mne.filter.filter_data(data[3], 256, 5, 15)
+        #filtered_data4 = mne.filter.filter_data(data[4], 256, 5, 15)
+        print(filtered_data)
