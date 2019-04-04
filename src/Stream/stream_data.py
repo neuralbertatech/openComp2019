0ee3390152637b336fbe44cf1dcb4aee75352d19
Eden <eredman@ualberta.ca>
2019-04-04 17:26:33 -0600

    Added threading

--- src/Stream/stream_data.py
+++ src/Stream/stream_data.py
@@ -6,6 +6,7 @@ from pylsl import StreamInlet, resolve_byprop
 from threading import Thread
 import mne
 import math
+from threading import Thread
 
 # Enough for 1 sec at 256 Hz
 BUFFER = 256
@@ -47,42 +48,73 @@ class CircularBuffer:
         self.window_write = (self.window_write + 1) % self.chunks
         self.window_read = (self.window_read + 1) % self.chunks
 
-times = []
-count = 0
-chunks = 3
-ave_len = 10
-ave = [0.0, 0.0]
-buf = CircularBuffer(chunks)
+#times = []
+#count = 0
+#chunks = 3
+#ave_len = 10
+#ave = [0.0, 0.0]
+#buf = CircularBuffer(chunks) 
+
+class Stream (Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        # Enough for 1 sec at 256 Hz                                                                               
+        self.BUFFER = 256
+        print("looking for an EEG stream...")
+        self.streams = resolve_byprop('type', 'EEG', timeout=2)
+        
+        if len(self.streams) == 0:
+            raise(RuntimeError("Cant find EEG stream"))
+        print("Start aquiring data")
+
+        self.stream = self.streams[0]
+
+        self.inlet = StreamInlet(self.stream, max_chunklen=BUFFER)
+        self.times = []
+        self.count = 0
+        self.chunks = 3
+        self.ave_len = 10
+        self.ave = [0.0, 0.0]
+        self.buf = CircularBuffer(self.chunks)
+        self.state = 'noise'
 
 
-while True:
-    # Sample is a 2d array of [ [channel_i]*5 ] * BUFFER
-    samples, timestamps = inlet.pull_chunk(timeout=2.0, max_samples=BUFFER)
-    if timestamps:
-        data = np.vstack(samples)
-        data = np.transpose(data)
-        print(np.shape(data))
-        buf.write(data)
+    def run(self):
 
-        # Check so that the buffer is filled before any filtering
-        if count >= chunks:
-            channels = list([0])
-            lower_freq = 8
-            higher_freq = 12
-            filtered_data = mne.filter.filter_data(buf.window, 256, lower_freq, higher_freq, filter_length=256*chunks-1, fir_design='firwin')
+        while True:
+            # Sample is a 2d array of [ [channel_i]*5 ] * BUFFER
+            samples, timestamps = self.inlet.pull_chunk(timeout=2.0, max_samples=self.BUFFER)
+            if timestamps:
+                data = np.vstack(samples)
+                data = np.transpose(data)
+                print(np.shape(data))
+                self.buf.write(data)
+
+            # Check so that the buffer is filled before any filtering
+            if self.count >= self.chunks:
+                channels = list([0])
+                lower_freq = 8
+                higher_freq = 12
+                filtered_data = mne.filter.filter_data(self.buf.window, 256, lower_freq, higher_freq, filter_length=256*self.chunks-1, fir_design='firwin')
             #for channel in filtered_data:
             #    print(alpha(channel))
-            if(count < chunks+ave_len):
-                ave[0] += alpha(filtered_data[1]) * (1./ave_len)
-                ave[1] += alpha(filtered_data[2]) * (1./ave_len)
-
-            elif(alpha(filtered_data[2]) > ave[1]*0.90):
-                if(alpha(filtered_data[1]) > ave[0]*1.05):
-                    print('Noise')
+                if(self.count < self.chunks+self.ave_len):
+                    self.ave[0] += alpha(filtered_data[1]) * (1./self.ave_len)
+                    self.ave[1] += alpha(filtered_data[2]) * (1./self.ave_len)
+                
+                elif(alpha(filtered_data[2]) > self.ave[1]*0.90):
+                    if(alpha(filtered_data[1]) > self.ave[0]*1.05):
+                        #print('Noise')
+                        self.state = 'noise'
+                    else:
+                        self.state = 'yes'
+                        
                 else:
-                    print('Yes')
+                    self.state = 'no'
+                
+            self.count += 1
 
-            else:
-                print('No')
 
-        count += 1
+if __name__ == '__main__':
+    t = Stream()
+    t.start()
